ac07e79d4fa59652b4ba3f204816b67840c100c1
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-10 21:46:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ CHARACTER_DIMENSIONS = {
 def generate_image(text):
     # Calculate image dimensions
     total_width = sum(
-        max(CHARACTER_DIMENSIONS.get(char_set, (0, 50))[0] for char_set in CHARACTER_DIMENSIONS if char in char_set) for
-        char in text)
+        max(CHARACTER_DIMENSIONS.get(char_set, (50, 50))[0] for char_set in CHARACTER_DIMENSIONS if char in char_set)
+        for char in text)
     img_height = 50
 
     # Create a transparent image with RGBA mode
@@ -31,24 +31,31 @@ def generate_image(text):
     x = 0
 
     for char in text:
-        char_img_path = os.path.join('static', f"{char}.png")
-        try:
-            char_img = Image.open(char_img_path).convert('RGBA')
-            char_width, _ = char_img.size
+        if char == ' ':
+            char_width, _ = CHARACTER_DIMENSIONS[char]
+        else:
+            char_img_path = os.path.join('static', f"{char}.png")
+            try:
+                char_img = Image.open(char_img_path).convert('RGBA')
+                char_width, _ = char_img.size
 
-            # Find the character set to get the appropriate width
-            char_set = next((char_set for char_set in CHARACTER_DIMENSIONS if char in char_set), None)
-            if char_set:
-                char_set_width, _ = CHARACTER_DIMENSIONS[char_set]
-                char_scale_factor = char_set_width / char_width
-                char_width *= char_scale_factor
+                # Find the character set to get the appropriate width
+                char_set = next((char_set for char_set in CHARACTER_DIMENSIONS if char in char_set), None)
+                if char_set:
+                    char_set_width, _ = CHARACTER_DIMENSIONS[char_set]
+                    char_scale_factor = char_set_width / char_width
+                    char_width *= char_scale_factor
+                else:
+                    # Use a default width if the character is not in any defined character set
+                    char_width, _ = CHARACTER_DIMENSIONS.get(char, (50, 50))
 
-            # Resize the character image to the appropriate width and paste it
-            char_img = char_img.resize((int(char_width), img_height), Image.BILINEAR)
-            img.paste(char_img, (x, 0), char_img)
-            x += int(char_width)
-        except Exception as e:
-            raise ValueError(f"Error processing character '{char}': {str(e)}")
+                # Resize the character image to the appropriate width and paste it
+                char_img = char_img.resize((int(char_width), img_height), Image.BILINEAR)
+                img.paste(char_img, (x, 0), char_img)
+            except Exception as e:
+                raise ValueError(f"Error processing character '{char}': {str(e)}")
+
+        x += int(char_width)
 
     return img
 
