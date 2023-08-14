702801866ea454fae857b2f39c61cf24d1a313d6
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-14 23:49:32 +0100

    Fixed Some Issues

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-ALLOWED_CHARACTERS = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ!?0123456789 ")
-CHARACTER_FOLDERS = {
-    '?': 'Symbols/Question',
-    '!': 'Symbols/Exclamation',
-}
+# Define allowed character sets
+ALLOWED_ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ALLOWED_NUMBERS = "123456789"
+ALLOWED_SYMBOLS = "?!"
+ALLOWED_CHARACTERS = ALLOWED_ALPHABETS + ALLOWED_NUMBERS + ALLOWED_SYMBOLS
+
+# Folder names
+SYMBOLS_FOLDER = 'Symbols'
 CHARACTER_FOLDER = 'Alphabets'
 NUMBER_FOLDER = 'Numbers'
-SPACE_WIDTH = 20
+
+# Dimensions
+SPACE_WIDTH = 10
 GENERATED_IMAGE_PATH = 'result.png'
 
 def generate_image(text):
     try:
+        # Sanitize input text
         sanitized_text = ''.join(char for char in text if char in ALLOWED_CHARACTERS)
 
         img_height = None
@@ -25,22 +31,19 @@ def generate_image(text):
             if char == ' ':
                 if chars and chars[-1][1] == ' ':
                     continue
+                # Create space image for formatting
                 char_img = Image.new('RGBA', (SPACE_WIDTH, 1), (0, 0, 0, 0))
                 char_width = SPACE_WIDTH
             else:
-                if char in CHARACTER_FOLDERS:
-                    char_img_path = os.path.join('static', CHARACTER_FOLDERS[char] + '.png')
-                elif char.isdigit():
-                    char_img_path = os.path.join('static', NUMBER_FOLDER, char + '.png')
-                else:
-                    char_img_path = os.path.join('static', CHARACTER_FOLDER, char + '.png')
-                
+                # Get image path for the character
+                char_img_path = get_character_image_path(char)
                 with Image.open(char_img_path).convert('RGBA') as char_img:
                     char_size = char_img.size
                     char_width = char_size[0]
                     img_height = char_size[1] if img_height is None else img_height
             chars.append((char_img, char_width))
 
+        # Combine character images into a single image
         total_width = sum(char_width for _, char_width in chars)
         img = Image.new('RGBA', (total_width, img_height), (0, 0, 0, 0))
         x = 0
@@ -48,6 +51,7 @@ def generate_image(text):
             img.paste(char_img, (x, 0), char_img)
             x += char_width
 
+        # Save the combined image
         img_path = os.path.join('static', GENERATED_IMAGE_PATH)
         img.save(img_path)
 
@@ -57,6 +61,18 @@ def generate_image(text):
         return None, "Some characters are not supported."
     except Exception as e:
         return None, f"An error occurred: {str(e)}"
+
+def get_character_image_path(char):
+    if char in ALLOWED_ALPHABETS:
+        return os.path.join('static', CHARACTER_FOLDER, char + '.png')
+    elif char in ALLOWED_NUMBERS:
+        return os.path.join('static', NUMBER_FOLDER, char + '.png')
+    elif char == '!':
+        return os.path.join('static', SYMBOLS_FOLDER, 'Exclamation.png')
+    elif char == '?':
+        return os.path.join('static', SYMBOLS_FOLDER, 'Question.png')
+    else:
+        raise ValueError(f"The character '{char}' is not supported.")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
