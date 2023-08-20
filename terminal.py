42cd204730bffde3f809072e42bdaac604b1f146
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-21 00:37:11 +0100

    Added A Warning

--- terminal.py
+++ terminal.py
@@ -1,28 +1,29 @@
 import sys
 import random
+from color import COLORS
+from main import generate_filename, generate_image_with_filename
+
 # Prevent the generation of __pycache__ directories
 sys.dont_write_bytecode = True
-from main import generate_filename, generate_image_with_filename 
-
-sys.dont_write_bytecode = True
-from color import COLORS, BOLD, RESET
 
 # Main function
 def main():
+    print("Note: Using the Metal Slug font style to turn text into images might not work perfectly with all fonts. Check The SUPPORTED.md file for more information")
+    
     while True:
-        text = input(random.choice(COLORS) + BOLD + "Enter the desired output (press Enter to quit): " + RESET)
+        text = input(random.choice(COLORS) + "Enter the desired output (press Enter to quit): ")
         
         if not text:
-            print(random.choice(COLORS) + BOLD + "Goodbye!" + RESET)
+            print(random.choice(COLORS) + "Goodbye!")
             break
 
         filename = generate_filename(text)
         img_path, error_message = generate_image_with_filename(text, filename)
 
         if error_message:
-            print(random.choice(COLORS) + error_message + RESET)
+            print(random.choice(COLORS) + error_message)
         else:
-            print(random.choice(COLORS) + BOLD + f"Image generated successfully: {img_path}" + RESET)
+            print(random.choice(COLORS) + f"Image generated successfully: {img_path}")
 
 if __name__ == "__main__":
     main()
