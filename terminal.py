eb253e29b37aabdbc0d2aa5354e5927c6d39d0ae
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-20 13:50:17 +0100

    Accidentally Removed Some Files 💀

---
+++ terminal.py
@@ -0,0 +1,27 @@
+import sys
+import random
+from app import generate_filename, generate_image_with_filename
+from terminal import COLORS, BOLD, RESET
+
+# Prevent the generation of __pycache__ directories
+sys.dont_write_bytecode = True
+
+# Main function
+def main():
+    while True:
+        text = input(random.choice(COLORS) + BOLD + "Enter the desired output (press Enter to quit): " + RESET)
+
+        if not text:
+            print(random.choice(COLORS) + BOLD + "Goodbye!" + RESET)
+            break
+
+        filename = generate_filename(text)
+        img_path, error_message = generate_image_with_filename(text, filename)
+
+        if error_message:
+            print(random.choice(COLORS) + error_message + RESET)
+        else:
+            print(random.choice(COLORS) + BOLD + f"Image generated successfully: {img_path}" + RESET)
+
+if __name__ == "__main__":
+    main()
