87086adcc13fdb487380e91c4071b45eff286bf6
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-20 23:19:38 +0100

    Improved code readability

--- color.py
+++ color.py
@@ -1,19 +1,8 @@
-from colorama import Fore, Style
+from colorama import Fore
+from itertools import combinations
 
-# Color codes
-COLORS = [
-    Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA,
-    Fore.CYAN, Fore.WHITE, Fore.LIGHTBLACK_EX, Fore.LIGHTRED_EX,
-    Fore.LIGHTGREEN_EX, Fore.LIGHTYELLOW_EX, Fore.LIGHTBLUE_EX,
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTWHITE_EX
-]
+# Available color codes
+COLORS = list(vars(Fore).values())
 
-# Text formatting options
-BOLD = Style.BRIGHT
-RESET = Style.RESET_ALL
-
-def generate_unique_color_combinations(colors):
-    combinations = [(color1, color2) for i, color1 in enumerate(colors) for j, color2 in enumerate(colors) if i != j]
-    return combinations
-
-DISTINCT_COLOR_COMBINATIONS = generate_unique_color_combinations(COLORS)
+# Generate distinct color combinations
+DISTINCT_COLOR_COMBINATIONS = list(combinations(COLORS, 2))
