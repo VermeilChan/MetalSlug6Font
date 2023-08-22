67d085d011c27b957a66f850d35f43ea5aa0f47f
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-22 15:41:49 +0100

    Refactor: Improve Code Readability

--- terminal.py
+++ terminal.py
@@ -1,29 +1,37 @@
 import sys
 import random
-from colorama import Fore
-from color import COLORS
-from main import generate_filename, generate_image_with_filename
+import traceback
+from color import COLORS  # Importing COLORS from the 'color' module
+from colorama import Fore  # Importing the 'Fore' class from the 'colorama' module
+from main import generate_filename, generate_image_with_filename, get_font_paths, font
 
-# Prevent the generation of __pycache__ directories
-sys.dont_write_bytecode = True
+sys.dont_write_bytecode = True  # Disable writing compiled bytecode files
 
-# Main function
 def main():
+    # Print a note about compatibility
     print(Fore.RED + "Note: Metal Slug font style conversion may not be compatible with all fonts. Refer to the SUPPORTED.md file for details.")
+    
+    # Start an infinite loop for user input
     while True:
-        text = input(random.choice(COLORS) + "Enter Text: ")
+        color_choice = random.choice(COLORS)  # Choose a random color from the COLORS list
+        text = input(f"{color_choice}Enter Text: ")  # Prompt the user for input
         
         if not text:
-            print(random.choice(COLORS) + "Goodbye!")
+            print(f"{color_choice}Goodbye!")  # Exit loop if no input is provided
             break
 
-        filename = generate_filename(text)
-        img_path, error_message = generate_image_with_filename(text, filename)
-
-        if error_message:
-            print(random.choice(COLORS) + error_message)
-        else:
-            print(random.choice(COLORS) + f"Image generated successfully: {img_path}")
+        try:
+            filename = generate_filename(text)  # Generate a filename based on the entered text
+            font_paths = get_font_paths(font)  # Get the paths of available fonts
+            img_path, error_message = generate_image_with_filename(text, filename, font_paths)  # Generate an image with the given text and font
+            
+            if error_message:
+                print(f"{color_choice}Error: {error_message}")  # Print error message if there's an issue
+            else:
+                print(f"{color_choice}Image generated successfully: {img_path}")  # Print success message with image path
+        except Exception as e:
+            print(f"{color_choice}An error occurred: {e}")  # Print an error message if an exception occurs
+            traceback.print_exc()  # Print the traceback for debugging purposes
 
 if __name__ == "__main__":
-    main()
+    main()  # Execute the 'main' function when the script is run directly
