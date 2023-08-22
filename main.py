d3eb853963c719a55208ad1422a70a0f0c9ce0ef
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-22 15:37:42 +0100

    Refactor and Enhance Font Text Image Generator

--- main.py
+++ main.py
@@ -2,172 +2,120 @@ import os
 import re
 import PIL
 import string
-from color import *
 from PIL import Image
 from pathlib import Path
-from colorama import init
+from colorama import Fore
 from datetime import datetime
 
-# Constants
-ALLOWED_ALPHABETS = string.ascii_letters
-ALLOWED_NUMBERS = string.digits
-ALLOWED_SYMBOLS = string.punctuation
-ALLOWED_CHARACTERS = set(ALLOWED_ALPHABETS + ALLOWED_NUMBERS + ALLOWED_SYMBOLS + ' ')
+# Set of allowed characters for the generated image
+ALLOWED_CHARACTERS = set(string.ascii_letters + string.digits + string.punctuation + ' ')
 
-def GET(font):
-
-    if font == 1:
-            CHAR = f'Assets\\Font-1\\Letters'
-            SYB  = f'Assets\\Font-1\\Numbers'
-            NUM  = f'Assets\\Font-1\\Symbols'
-
-    if font == 2:
-            CHAR = f'Assets\\Font-2\\Letters'
-            SYB  = f'Assets\\Font-2\\Numbers'
-            NUM  = f'Assets\\Font-2\\Symbols'
-
-    if font == 3:
-            CHAR = f'Assets\\Font-3\\Letters'
-            SYB  = f'Assets\\Font-3\\Numbers'
-            NUM  = f'Assets\\Font-3\\Symbols'
-
-    if font == 4:
-            CHAR = f'Assets\\Font-4\\Letters'
-            SYB  = f'Assets\\Font-4\\Numbers'
-            NUM  = f'Assets\\Font-4\\Symbols'
-
-
-    return (CHAR , SYB , NUM)
-
+# Width of a space character in pixels
 SPACE_WIDTH = 10
 
-# Initialize colorama
-init(autoreset=True)
-
-# Get the user's desktop folder
+# Function to get the user's desktop path
 def get_desktop_path():
     desktop = Path.home() / "Desktop"
     return str(desktop)
 
-# Generate a unique filename based on user input and current date and time
+# Function to generate a filename based on user input and current timestamp
 def generate_filename(user_input):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    input = re.sub(r'[^a-zA-Z0-9]', '_', user_input)
-    filename = f"{input}_{timestamp}.png"
-    return filename
+    sanitized_input = re.sub(r'\W+', '_', user_input)
+    return f"{sanitized_input}_{timestamp}.png"
 
-font = int(input(f"{Fore.GREEN}Chose A Font From 1 To 4 : {Fore.RESET}"))
+# Get user input for font choice
+font = int(input(f"{Fore.GREEN}Choose A Font From 1 To 4 : {Fore.RESET}"))
 
-# Get the path of the character image based on the character
-def get_character_image_path(char:str):
-    f = GET(font)
+# Function to get paths to font resources
+def get_font_paths(font):
+    base_path = f'Assets/Font-{font}'
+    return (
+        os.path.join(base_path, 'Letters'),
+        os.path.join(base_path, 'Numbers'),
+        os.path.join(base_path, 'Symbols')
+    )
 
-    CHARACTERS_FOLDER = f[0]
-    NUMBERS_FOLDER    = f[1]
-    SYMBOLS_FOLDER    = f[2]
+# Function to get the image path for a specific character
+def get_character_image_path(char, font_paths):
+    CHARACTERS_FOLDER, NUMBERS_FOLDER, SYMBOLS_FOLDER = font_paths
 
-    if char in ALLOWED_ALPHABETS:
-        if char.islower():
-            return os.path.join(CHARACTERS_FOLDER+'\\Lower-Case', char + '.png')
-        else:
-            return os.path.join(CHARACTERS_FOLDER+'\\Upper-Case', char + '.png')
-    
-    elif char in ALLOWED_NUMBERS:
+    if char.isalpha():
+        folder = 'Lower-Case' if char.islower() else 'Upper-Case'
+        return os.path.join(CHARACTERS_FOLDER, folder, char + '.png')
+    elif char.isdigit():
         return os.path.join(NUMBERS_FOLDER, char + '.png')
-    if char == '!':
-        return os.path.join(SYMBOLS_FOLDER, 'Exclamation.png')
-    elif char == '?':
-        return os.path.join(SYMBOLS_FOLDER, 'Question.png')
-    elif char == "'":
-        return os.path.join(SYMBOLS_FOLDER, 'Apostrophe.png')
-    elif char == '*':
-        return os.path.join(SYMBOLS_FOLDER, 'Asterisk.png')
-    elif char == ')':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Left.png')
-    elif char == '}':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Left-2.png')
-    elif char == ']':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Left-3.png')
-    elif char == '(':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Right.png')
-    elif char == '{':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Right-2.png')
-    elif char == '[':
-        return os.path.join(SYMBOLS_FOLDER, 'Bracket-Right-3.png')
-    elif char == '^':
-        return os.path.join(SYMBOLS_FOLDER, 'Caret.png')
-    elif char == ':':
-        return os.path.join(SYMBOLS_FOLDER, 'Colon.png')
-    elif char == '$':
-        return os.path.join(SYMBOLS_FOLDER, 'Dollar.png')
-    elif char == '=':
-        return os.path.join(SYMBOLS_FOLDER, 'Equals.png')
-    elif char == '>':
-        return os.path.join(SYMBOLS_FOLDER, 'Greater-than.png')
-    elif char == '-':
-        return os.path.join(SYMBOLS_FOLDER, 'Hyphen.png')
-    elif char == '∞':
-        return os.path.join(SYMBOLS_FOLDER, 'Infinity.png')
-    elif char == '<':
-        return os.path.join(SYMBOLS_FOLDER, 'Less-than.png')
-    elif char == '#':
-        return os.path.join(SYMBOLS_FOLDER, 'Number.png')
-    elif char == '%':
-        return os.path.join(SYMBOLS_FOLDER, 'Percent.png')
-    elif char == '.':
-        return os.path.join(SYMBOLS_FOLDER, 'Period.png')
-    elif char == '+':
-        return os.path.join(SYMBOLS_FOLDER, 'Plus.png')
     elif char in string.punctuation:
-        return os.path.join(SYMBOLS_FOLDER, 'Punctuation.png')
-    elif char == '"':
-        return os.path.join(SYMBOLS_FOLDER, 'Quotation.png')
-    elif char == ';':
-        return os.path.join(SYMBOLS_FOLDER, 'Semicolon.png')
-    elif char == '/':
-        return os.path.join(SYMBOLS_FOLDER, 'Slash.png')
-    elif char == '~':
-        return os.path.join(SYMBOLS_FOLDER, 'Tilde.png')
-    elif char == '_':
-        return os.path.join(SYMBOLS_FOLDER, 'Underscore.png')
-    elif char == '|':
-        return os.path.join(SYMBOLS_FOLDER, 'Vertical-bar.png')
+        return os.path.join(SYMBOLS_FOLDER, char + '.png')
+    elif char == ' ':
+        return None
     else:
-        raise ValueError(f"The character '{char}' is not supported.")
+        # Special characters mapping
+        SPE_CHAR = {
+            '!': 'Exclamation',
+            '?': 'Question',
+            "'": 'Apostrophe',
+            '*': 'Asterisk',
+            ')': 'Bracket-Left',
+            '}': 'Bracket-Left-2',
+            ']': 'Bracket-Left-3',
+            '(': 'Bracket-Right',
+            '{': 'Bracket-Right-2',
+            '[': 'Bracket-Right-3',
+            '^': 'Caret',
+            ':': 'Colon',
+            '$': 'Dollar',
+            '=': 'Equals',
+            '>': 'Greater-than',
+            '-': 'Hyphen',
+            '∞': 'Infinity',
+            '<': 'Less-than',
+            '#': 'Number',
+            '%': 'Percent',
+            '.': 'Period',
+            '+': 'Plus',
+            '"': 'Quotation',
+            ';': 'Semicolon',
+            '/': 'Slash',
+            '~': 'Tilde',
+            '_': 'Underscore',
+            '|': 'Vertical-bar'
+        }
+        if char in SPE_CHAR:
+            return os.path.join(SYMBOLS_FOLDER, f"{SPE_CHAR[char]}.png")
+        else:
+            raise ValueError(f"The character '{char}' is not supported.")
 
-# Generate an image from the provided text and save it with the provided filename
-def generate_image_with_filename(text, filename):
+# Function to generate an image based on input text, filename, and font paths
+def generate_image_with_filename(text, filename, font_paths):
     try:
+        # Remove any characters not in the allowed set
         text = ''.join(char for char in text if char in ALLOWED_CHARACTERS)
         img_height = None
         chars = []
 
+        # Generate a list of character images and widths
         for char in text:
             if char == ' ':
-                # Handle spaces by creating a transparent image
                 if chars and chars[-1][1] == ' ':
                     continue
                 char_img = Image.new('RGBA', (SPACE_WIDTH, 1), (0, 0, 0, 0))
                 char_width = SPACE_WIDTH
-
             else:
-                char_img_path = get_character_image_path(char)
+                char_img_path = get_character_image_path(char, font_paths)
                 try:
-
-                    with Image.open(char_img_path).convert('RGBA') as char_img:
-                        char_size = char_img.size
-                        char_width = char_size[0]
-                        img_height = char_size[1] if img_height is None else img_height
-
+                    char_img = Image.open(char_img_path).convert('RGBA')
+                    char_size = char_img.size
+                    char_width = char_size[0]
+                    img_height = char_size[1] if img_height is None else img_height
                 except FileNotFoundError as fnfe:
                     raise ValueError(f"Image file not found: {str(fnfe)}")
-                
                 except PIL.Image.Error as img_err:
                     raise ValueError(f"Error processing image: {str(img_err)}")
-                
+
             chars.append((char_img, char_width))
 
-        # Create a composite image by pasting individual character images
+        # Calculate total width of the image
         total_width = sum(char_width for _, char_width in chars)
         img = Image.new('RGBA', (total_width, img_height), (0, 0, 0, 0))
         x = 0
@@ -175,10 +123,9 @@ def generate_image_with_filename(text, filename):
             img.paste(char_img, (x, 0), char_img)
             x += char_width
 
-        # Get the user's desktop path and save the composite image
+        # Save the generated image to the user's desktop
         desktop_path = get_desktop_path()
         img_path = os.path.join(desktop_path, filename)
-
         img.save(img_path)
 
         return filename, None
