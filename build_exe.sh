362055a966b2ab5dbaf67143f294478935cfa175
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-23 15:56:56 +0100

    Commit

--- build_exe.sh
+++ build_exe.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd "/home/runner/work/MetalSlugFont/MetalSlugFont"
-
 pyinstaller --noconfirm --onedir --console --icon "Assets/Sv-001.ico" --name "MetalSlugFont" --add-data "color.py" --add-data "main.py" --add-data "Assets;Assets/"  "terminal.py"
