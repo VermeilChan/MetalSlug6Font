deb598cd3c6173e69d692a3d4fa75b13b5fcf845
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-23 13:32:13 +0100

    4

--- build_exe.sh
+++ build_exe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --noconfirm --onedir --console --icon "Assets/Sv-001.ico" --name "MetalSlugFont" --add-data "Assets/Assets:Assets" --add-data "color.py:." --add-data "main.py:." "terminal.py"
+pyinstaller --noconfirm --onedir --console --icon "Assets/Sv-001.ico" --name "MetalSlugFont" --add-data "Assets:Assets" --add-data "color.py:." --add-data "main.py:." "terminal.py"
