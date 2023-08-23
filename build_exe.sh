b9ab8848e2b1e25238fe318307241e634399944f
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-23 16:32:00 +0100

    Removed build_exe.sh And Build.yml

--- build_exe.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pyinstaller --noconfirm --onedir --console --icon "Assets/Sv-001.ico" --name "MetalSlugFont" --add-data "Assets:Assets/" --add-data "color.py:." --add-data "main.py:."  "terminal.py"
