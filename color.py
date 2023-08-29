d585d9b866083d384971a281043cb6372c1a78af
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-29 23:36:08 +0100

    Removed Comments

--- color.py
+++ color.py
@@ -1,8 +1,6 @@
 from colorama import Fore
 from itertools import combinations
 
-# Available color codes excluding black
 COLORS = [color_code for color_name, color_code in vars(Fore).items() if color_name != 'BLACK']
 
-# Generate distinct color combinations
 DISTINCT_COLOR_COMBINATIONS = list(combinations(COLORS, 2))
