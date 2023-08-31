86a49640bad87a6064876b5ac86c24bcd6b6a306
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-08-31 22:28:38 +0100

    Removed color.py

--- color.py
+++
@@ -1,6 +0,0 @@
-from colorama import Fore
-from itertools import combinations
-
-COLORS = [color_code for color_name, color_code in vars(Fore).items() if color_name != 'BLACK']
-
-DISTINCT_COLOR_COMBINATIONS = list(combinations(COLORS, 2))
