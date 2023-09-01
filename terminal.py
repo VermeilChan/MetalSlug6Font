6a642cc7d0e41b668ad1673e24995f0fe93ecacb
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-09-01 09:48:22 +0100

    Small Changes

--- terminal.py
+++ terminal.py
@@ -1,24 +1,29 @@
 import sys
-import traceback
+import logging
+from main import generate_filename, generate_image_with_filename, get_font_paths, font, color
 
 # Prevent the generation of bytecode files (.pyc)
 sys.dont_write_bytecode = True
 
-# Import functions and variables from the 'main.py' file
-from main import generate_filename, generate_image_with_filename, get_font_paths, font, color
+# Initialize logging
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s')
 
 # Display an introductory message to the user
 def display_intro_message():
-    print("Note : Metal Slug font style conversion may not be compatible with all fonts."
-            " Refer to the SUPPORTED.md file for details.")
+    print("Note: Metal Slug font style conversion may not be compatible with all fonts.")
+    print("Refer to the SUPPORTED.md file for details.")
 
 # Get user input for the text they want to generate
 def get_user_input():
-    return input("Enter the text you want to generate: ")
+    return input("Enter the text you want to generate (type 'exit' to quit): ")
 
 # Generate an image from the user's text and display it or an error message
 def generate_and_display_image(text):
     try:
+        if text.lower() == 'exit':
+            print("Closing...")
+            sys.exit(0)
+
         # Generate a filename based on the user's input
         filename = generate_filename(text)
 
@@ -30,13 +35,13 @@ def generate_and_display_image(text):
 
         if error_message:
             print(f"Error: {error_message}")
+            logging.error(error_message)
         else:
             print(f"Successfully generated and saved: {img_path}")
     except Exception as e:
-        print(f"An error occurred: {e}")
-
-        # Print a traceback to help diagnose the error
-        traceback.print_exc()
+        error_message = f"An error occurred: {e}"
+        print(error_message)
+        logging.error(error_message, exc_info=True)
 
 # The main function that orchestrates the program
 def main():
@@ -44,11 +49,6 @@ def main():
 
     while True:
         text = get_user_input()
-
-        if not text:
-            print("Closing...")
-            break
-
         generate_and_display_image(text)
 
 if __name__ == "__main__":
