c0af24db186502cc25e6f5e78023ab0f4d493342
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-09-09 19:05:41 +0100

    Refactored the code to improve user experience, handle user exits gracefully, and added keyboard interrupt handling for smoother program termination

--- terminal.py
+++ terminal.py
@@ -2,25 +2,29 @@
 import sys
 import logging
 
-from main import generate_filename, generate_image_with_filename, get_font_paths
-
-# Prevent the generation of bytecode files (.pyc)
+# Prevent the generation of .pyc (Python bytecode) files
 sys.dont_write_bytecode = True
 
-# Initialize logging
-logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+# Import functions from another file named 'main'
+from main import generate_filename, generate_image_with_filename, get_font_paths
 
-# Display an introductory message to the user
+# Set up logging to a file named 'app.log'
+log_filename = 'app.log'
+log_format = '%(asctime)s - %(levelname)s: %(message)s'
+logging.basicConfig(filename=log_filename, level=logging.INFO, format=log_format)
+
+# Function to display an introductory message
 def display_intro_message():
     print("Note: Metal Slug Font style conversion may not be compatible with all fonts.")
     print("Refer to the SUPPORTED.md file for details.")
 
-# Get user input for the text they want to generate
+# Function to get user input for text to be converted
 def get_user_input():
     return input("Enter the text you want to generate (type 'exit' to close): ")
 
-# Get user input for font and color selection
+# Function to allow the user to select a font and color
 def select_font_and_color():
+    # Define valid color options for each font
     VALID_COLORS_BY_FONT = {
         1: ["Blue", "Orange-1", "Orange-2"],
         2: ["Blue", "Orange-1", "Orange-2"],
@@ -31,16 +35,27 @@ def select_font_and_color():
 
     while True:
         try:
-            font = int(input("Choose a font from 1 to 5 (Refer to EXAMPLE.md for Font Preview): "))
+            # Prompt the user to choose a font or exit
+            user_input = input("Choose a font from 1 to 5 (Refer to EXAMPLE.md for Font Preview) or type 'exit' to close: ")
 
+            if user_input.lower() == 'exit':
+                print("Closing...")
+                sys.exit(0)
+
+            font = int(user_input)
+
+            # Check if the chosen font is valid
             if font in VALID_COLORS_BY_FONT:
                 valid_colors = [color.lower() for color in VALID_COLORS_BY_FONT[font]]
                 print("Available colors: " + " | ".join(valid_colors))
-                color = input("Enter the color you want to use: ").lower()
+                color_input = input("Enter the color you want to use or type 'exit' to close: ").lower()
 
-                if color in valid_colors:
-                    logging.info(f"Chosen Font: {font}, Chosen Color: {color}")
-                    return font, color
+                if color_input == 'exit':
+                    print("Closing...")
+                    sys.exit(0)
+                elif color_input in valid_colors:
+                    logging.info(f"Chosen Font: {font}, Chosen Color: {color_input}")
+                    return font, color_input
                 else:
                     print("Invalid color. Please choose a valid color.")
 
@@ -50,20 +65,17 @@ def select_font_and_color():
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-# Generate an image from the user's text and display it or an error message
+# Function to generate and display an image based on user input
 def generate_and_display_image(text, font, color):
     try:
         if text.lower() == 'exit':
             print("Closing...")
             sys.exit(0)
 
-        # Generate a filename based on the user's input
         filename = generate_filename(text)
 
-        # Get font paths based on user-specified font and color
         font_paths = get_font_paths(font, color)
 
-        # Generate the image and handle any potential errors
         img_path, error_message = generate_image_with_filename(text, filename, font_paths)
 
         if error_message:
@@ -72,21 +84,46 @@ def generate_and_display_image(text, font, color):
         else:
             print(f"Image successfully generated and saved as: {img_path}")
             logging.info(f"Generated Image Path: {img_path}")
+    except KeyboardInterrupt:
+        print("Closing...")
+        sys.exit(0)
     except Exception as e:
         error_message = f"An error occurred: {e}"
         print(error_message)
         logging.exception(error_message)
 
-# The main function that orchestrates the program
+# The main function of the program
 def main():
     display_intro_message()
-    
-    # Ask for font and color once at the beginning
-    font, color = select_font_and_color() 
-    
-    while True:
-        text = get_user_input()
-        generate_and_display_image(text, font, color)
 
+    font, color = select_font_and_color() 
+
+    try:
+        while True:
+            text = get_user_input()
+            generate_and_display_image(text, font, color)
+    except KeyboardInterrupt:
+        print("Closing...")
+        sys.exit(0)
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {e}"
+        print(error_message)
+        logging.exception(error_message)
+
+    finally:
+        logging.shutdown()
+
+# Entry point of the script
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Closing...")
+        sys.exit(0)
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {e}"
+        print(error_message)
+        logging.exception(error_message)
+
+    finally:
+        logging.shutdown()
