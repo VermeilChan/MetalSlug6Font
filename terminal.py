87ce0d806885b2690ca0199cf0456b77c0288019
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-09-10 13:38:03 +0100

    Updated logging configuration and improved user input handling

--- terminal.py
+++ terminal.py
@@ -1,6 +1,7 @@
 # Import necessary libraries
 import sys
 import logging
+from logging.handlers import RotatingFileHandler
 
 # Prevent the generation of .pyc (Python bytecode) files
 sys.dont_write_bytecode = True
@@ -8,15 +9,23 @@ sys.dont_write_bytecode = True
 # Import functions from another file named 'main'
 from main import generate_filename, generate_image_with_filename, get_font_paths
 
-# Set up logging to a file named 'app.log'
+# Set up logging to a rotating log file named 'app.log' with a maximum size of 1 MB
 log_filename = 'app.log'
 log_format = '%(asctime)s - %(levelname)s: %(message)s'
-logging.basicConfig(filename=log_filename, level=logging.INFO, format=log_format)
+
+# Use a RotatingFileHandler to enable log rotation
+log_handler = RotatingFileHandler(log_filename, maxBytes=1e6, backupCount=5)
+log_handler.setFormatter(logging.Formatter(log_format))
+
+# Create a logger and set the level to INFO
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
 
 # Function to display an introductory message
 def display_intro_message():
-    print("Note: Metal Slug Font style conversion may not be compatible with all fonts.")
-    print("Refer to the SUPPORTED.md file for details.")
+    logger.info("Note: Metal Slug Font style conversion may not be compatible with all fonts.")
+    logger.info("Refer to the SUPPORTED.md file for details.")
 
 # Function to get user input for text to be converted
 def get_user_input():
@@ -46,15 +55,16 @@ def select_font_and_color():
 
             # Check if the chosen font is valid
             if font in VALID_COLORS_BY_FONT:
-                valid_colors = [color.lower() for color in VALID_COLORS_BY_FONT[font]]
+                valid_colors = VALID_COLORS_BY_FONT[font]
                 print("Available colors: " + " | ".join(valid_colors))
-                color_input = input("Enter the color you want to use or type 'exit' to close: ").lower()
+                color_input = input("Enter the color you want to use or type 'exit' to close: ")
 
-                if color_input == 'exit':
+                if color_input.lower() == 'exit':
                     print("Closing...")
                     sys.exit(0)
-                elif color_input in valid_colors:
-                    logging.info(f"Chosen Font: {font}, Chosen Color: {color_input}")
+                elif color_input.title() in valid_colors:
+                    color_input = color_input.title()
+                    logger.info(f"Chosen Font: {font}, Chosen Color: {color_input}")
                     return font, color_input
                 else:
                     print("Invalid color. Please choose a valid color.")
@@ -80,23 +90,27 @@ def generate_and_display_image(text, font, color):
 
         if error_message:
             print(f"Error: {error_message}")
-            logging.error(error_message)
+            logger.error(error_message)
         else:
             print(f"Image successfully generated and saved as: {img_path}")
-            logging.info(f"Generated Image Path: {img_path}")
+            logger.info(f"Generated Image Path: '{img_path}'")
+
+        # Log chosen font, chosen color, and user input
+        logger.info(f"Chosen Font: {font}, Chosen Color: {color}, User Input: {text}")
+
     except KeyboardInterrupt:
         print("Closing...")
         sys.exit(0)
     except Exception as e:
         error_message = f"An error occurred: {e}"
         print(error_message)
-        logging.exception(error_message)
+        logger.exception(error_message)
 
 # The main function of the program
 def main():
     display_intro_message()
 
-    font, color = select_font_and_color() 
+    font, color = select_font_and_color()
 
     try:
         while True:
@@ -108,7 +122,7 @@ def main():
     except Exception as e:
         error_message = f"An unexpected error occurred: {e}"
         print(error_message)
-        logging.exception(error_message)
+        logger.exception(error_message)
 
     finally:
         logging.shutdown()
@@ -123,7 +137,7 @@ if __name__ == "__main__":
     except Exception as e:
         error_message = f"An unexpected error occurred: {e}"
         print(error_message)
-        logging.exception(error_message)
+        logger.exception(error_message)
 
     finally:
         logging.shutdown()
