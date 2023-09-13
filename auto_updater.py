c1744a41c8efec60b9520d8e64ac260fb598ed60
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2023-09-13 16:26:24 +0100

    Refactor update-checking code for better structure and user experience

--- auto_updater.py
+++ auto_updater.py
@@ -1,7 +1,10 @@
 import os
+import time
+import click
+import shutil
 import logging
 import requests
-import time
+from logging.handlers import RotatingFileHandler
 
 from tqdm import tqdm
 
@@ -9,63 +12,57 @@ from tqdm import tqdm
 GITHUB_OWNER = 'VermeilChan'
 GITHUB_REPO = 'MetalSlugFont'
 RELEASE_FILE_EXTENSION = '.exe'
-CURRENT_VERSION = '0.2.5'
-DOWNLOAD_FOLDER = os.path.expanduser('~/Downloads')
+CURRENT_VERSION = '0.2.6'
 LOG_FILE = 'updates.log'
-GITHUB_API_URL = f'https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/releases/latest'
-DOWNLOAD_URL = None
+DOWNLOAD_FOLDER = os.path.expanduser('~/Downloads')
 
-# Configure logging
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.INFO,
-    format='%(asctime)s [%(levelname)s]: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+# Configure logging with log rotation
+logger = logging.getLogger('updates')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(LOG_FILE, maxBytes=10 * 1024 * 1024, backupCount=5)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Custom exception for rate limit exceeded
+class RateLimitExceededError(Exception):
+    def __init__(self, sleep_time):
+        self.sleep_time = sleep_time
 
 # Function to check for updates
-def check_for_updates():
+def check_for_updates(update_folder):
     try:
         latest_version, download_url = get_latest_version_and_download_url()
-        
+
         if latest_version == CURRENT_VERSION:
-            print(f"You are currently running version {CURRENT_VERSION}, which is up to date.")
-            logging.info(f"Running version {CURRENT_VERSION}. No updates are available.")
+            logger.info(f"You are currently running version {CURRENT_VERSION}, which is up to date.")
+            click.echo(f"You are currently running version {CURRENT_VERSION}, which is up to date.")
         else:
-            while True:
-                update_confirmation = input(f"You are currently running version {CURRENT_VERSION}. Do you want to update to version {latest_version}? (yes/no): ").strip().lower()
-                if update_confirmation in ('yes', 'y'):
-                    if is_update_file_exist(download_url):
-                        while True:
-                            update_file_confirmation = input("A file with the same name already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
-                            if update_file_confirmation in ('yes', 'y'):
-                                download_update(download_url, latest_version)
-                                break
-                            elif update_file_confirmation in ('no', 'n'):
-                                print("Update canceled.")
-                                logging.info("Update canceled by the user.")
-                                break
-                            else:
-                                print("Invalid input. Please enter 'yes' or 'no'.")
-                                logging.warning("Invalid input for update file confirmation.")
-                    else:
-                        download_update(download_url, latest_version)
-                    break
-                elif update_confirmation in ('no', 'n'):
-                    print("Update canceled.")
-                    logging.info("Update canceled by the user.")
-                    break
-                else:
-                    print("Invalid input. Please enter 'yes' or 'no'.")
-                    logging.warning("Invalid input for update confirmation.")
+            update_confirmation = click.confirm(f"You are currently running version {CURRENT_VERSION}. Do you want to update to version {latest_version}?")
+
+            if update_confirmation:
+                if is_update_file_exist(download_url):
+                    update_file_confirmation = click.confirm("A file with the same name already exists. Do you want to overwrite it?")
+                    if not update_file_confirmation:
+                        click.echo("Update canceled.")
+                        logger.info("Update canceled by the user.")
+                        return
+                download_update(download_url, latest_version, update_folder)
+            else:
+                click.echo("Update canceled.")
+                logger.info("Update canceled by the user.")
     except requests.exceptions.RequestException as e:
         handle_error("Failed to check for updates. Please check your internet connection.", e)
+    except RateLimitExceededError as e:
+        click.echo(f"Rate limit exceeded. Sleeping for {e.sleep_time:.0f} seconds until the rate limit is reset.")
+        time.sleep(e.sleep_time)
+        check_for_updates(update_folder)
     except Exception as e:
         handle_error("An unexpected error occurred while checking for updates.", e)
 
 # Function to check if the download folder exists and is accessible
-def is_download_folder_available():
-    return os.path.isdir(DOWNLOAD_FOLDER)
+def is_download_folder_available(download_folder):
+    return os.path.isdir(download_folder)
 
 # Function to check if the update file already exists
 def is_update_file_exist(download_url):
@@ -76,25 +73,24 @@ def is_update_file_exist(download_url):
 def get_latest_version_and_download_url():
     try:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
         }
 
-        response = requests.get(GITHUB_API_URL, headers=headers, verify=True)  # Use HTTPS with SSL certificate verification
+        response = requests.get(f'https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/releases/latest', headers=headers, verify=True)
         response.raise_for_status()
-        
-        # Handle rate limiting
+
+        # Check rate limiting headers
         remaining_requests = int(response.headers.get('X-RateLimit-Remaining', 0))
+        reset_timestamp = int(response.headers.get('X-RateLimit-Reset', 0))
+        current_time = time.time()
+
         if remaining_requests <= 0:
-            reset_timestamp = int(response.headers.get('X-RateLimit-Reset', 0))
-            current_time = time.time()
             sleep_time = max(0, reset_timestamp - current_time)
-            print(f"Rate limit exceeded. Sleeping for {sleep_time:.0f} seconds until the rate limit is reset.")
-            time.sleep(sleep_time)
-            return get_latest_version_and_download_url()  # Retry the request after sleeping
-        
+            raise RateLimitExceededError(sleep_time)
+
         release_data = response.json()
         latest_version = release_data['tag_name']
-        
+
         if latest_version != CURRENT_VERSION:
             download_url = get_download_url(release_data)
             return latest_version, download_url
@@ -112,29 +108,13 @@ def get_download_url(release_data):
             return asset['browser_download_url']
 
 # Function to download and update the program
-def download_update(download_url, latest_version):
+def download_update(download_url, latest_version, update_folder):
     try:
-        # Create the download folder if it doesn't exist
-        os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+        os.makedirs(update_folder, exist_ok=True)
+        download_path = os.path.join(update_folder, os.path.basename(download_url))
+        temp_download_path = download_path + '.temp'
 
-        # Define the path for the downloaded file (using the original file name)
-        download_path = os.path.join(DOWNLOAD_FOLDER, os.path.basename(download_url))
-        
-        if os.path.exists(download_path):
-            while True:
-                update_file_confirmation = input("Are you sure you want to overwrite it? (yes/no): ").strip().lower()
-                if update_file_confirmation in ('yes', 'y'):
-                    break
-                elif update_file_confirmation in ('no', 'n'):
-                    print("Update canceled.")
-                    logging.info("Update canceled by the user.")
-                    return
-                else:
-                    print("Invalid input. Please enter 'yes' or 'no'.")
-                    logging.warning("Invalid input for update file confirmation.")
-
-        # Download the update with a progress bar
-        with requests.get(download_url, stream=True, verify=True) as response, open(download_path, 'wb') as outfile:
+        with requests.get(download_url, stream=True, verify=True) as response, open(temp_download_path, 'ab') as outfile:
             response.raise_for_status()
             total_size = int(response.headers.get('content-length', 0))
             block_size = 1024  # 1 KB
@@ -146,15 +126,16 @@ def download_update(download_url, latest_version):
 
             progress_bar.close()
 
-        print(f"\nUpdate downloaded to: {download_path}")
+        shutil.move(temp_download_path, download_path)
 
-        # Log the update
+        logger.info(f"Update downloaded to: {download_path}")
+        click.echo(f"Update downloaded to: {download_path}")
+
         log_update(latest_version)
 
-        # Print post-update instructions
-        print("\nUpdate complete. Please close the application.")
-        print("Go to your downloads folder and reinstall the program.")
-        print("Before that, remove the 'MSFONT' folder.\n")
+        click.echo("\nUpdate complete. Please close the application.")
+        click.echo("Go to your updates folder and reinstall the program.")
+        click.echo("Before that, remove the 'MSFONT' folder.\n")
     except requests.exceptions.RequestException as e:
         handle_error("Failed to download the update. Please check your internet connection.", e)
     except Exception as e:
@@ -167,7 +148,7 @@ def log_update(version):
             with open(LOG_FILE, 'a') as log:
                 log.write(f"Updated to version {version}\n")
         else:
-            print("The log file is not writable. The update could not be logged.")
+            click.echo("The log file is not writable. The update could not be logged.")
     except Exception as e:
         handle_error("Failed to log the update.", e)
 
@@ -177,19 +158,26 @@ def is_log_file_writable():
 
 # Function to handle errors
 def handle_error(message, error):
-    logging.error(f"{message}: {error}")
-    print(f"An error occurred: {error}")
+    logger.error(f"{message}: {error}")
+    click.echo(f"An error occurred: {error}")
 
-if __name__ == '__main__':
+# Main entry point with command-line arguments
+@click.command()
+@click.option('--update-folder', default='~/Downloads', help='Folder for storing updates.')
+def main(update_folder):
+    update_folder = os.path.expanduser(update_folder)
+
     while True:
-        user_input = input("Type 'Update' to check for updates or 'exit' to exit: ").strip().lower()
-        
+        user_input = click.prompt("Type 'Update' to check for updates or 'exit' to exit").strip().lower()
+
         if user_input == 'update':
-            check_for_updates()
+            check_for_updates(update_folder)
         elif user_input == 'exit':
-            print("Exiting the program.")
-            logging.info("Exiting the program.")
+            click.echo("Exiting the program...")
+            logger.info("Exiting the program...")
             break
         else:
-            print("Invalid input. Please type 'Update' to check for updates or 'exit' to exit.")
-            logging.warning("Invalid input from the user.")
+            click.echo("Invalid input. Please type 'Update' to check for updates or 'exit' to exit.")
+
+if __name__ == '__main__':
+    main()
